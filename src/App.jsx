8df746c747f609c2e4027a82a1a5eddcf671a105
Marcelinaidoko <mirabeluregu@gmail.com>
2024-12-14 08:24:35 +0100

    collaboration Page

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <>
     <BrowserRouter>
-         {/* <Header /> */}
+         <Header />
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/artisans" element={<Artisans/>} />
