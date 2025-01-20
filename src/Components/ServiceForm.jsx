b50cb2d8952bbcac3e5632286393ea1e1cc6ef1c
IdokoMarcelina <aitmacelina@gmail.com>
2025-01-20 21:04:15 +0100

    mimi you sef don try try try

--- src/Components/ServiceForm.jsx
+++ src/Components/ServiceForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './../Pages/ArtisanSignup.css';
-import signInImg from '../assets/images/signin-img.jpg';
+import signInImg from '../assets/images/signIn-img.jpg';
 import { useNavigate } from 'react-router-dom';
 
 const ServiceForm = () => {
